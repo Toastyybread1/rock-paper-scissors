54dd48a6bae11c96aea5290c25541971dc67c768
kevinhou7447@gmail.com <104533338+Toastyybread1@users.noreply.github.com>
2026-07-06 15:24:09 -0400

    updated dom

--- main.js
+++ main.js
@@ -3,6 +3,7 @@ let humanScore = 0;
 let computerScore = 0;
 
 let humanInput, computerInput; 
+let canPlay = true;
 
 function getComputerChoice() {
     const random = Math.random()
@@ -19,19 +20,10 @@ function getComputerChoice() {
     }
 }
 
-function getHumanChoice() {
-    let choice = window.prompt("Rock, Paper, or Scissors?");
-    if (choice.toLowerCase() == "rock") {
-        return "Rock";
-
-    } else if (choice.toLowerCase() == "paper") {
-        return "Paper";
-    } else if (choice.toLowerCase() == "scissors") {
-        return "Scissors";
-    }
+function updateScores() {
+    humanText.textContent = `Human score: ${humanScore}`;
+    computerText.textContent = `Computer score: ${computerScore}`;
 }
-
-
 
 function playRound(humanChoice, computerChoice) {
     
@@ -39,40 +31,99 @@ function playRound(humanChoice, computerChoice) {
     let humanInput = humanChoice;
     let computerInput = computerChoice;
 
-    alert("The computer plays... " + computerChoice + " !");
+    dashboard.textContent = `The computer chooses... ${computerChoice}!`;
 
     if (humanInput == computerInput) {
-        alert("Nobody won!");
-        playRound(getHumanChoice(), getComputerChoice())
-
+        console.log("nobody won!");
     } else if (humanInput == "Rock" && computerInput == "Scissors") {
-        humanScore += 1; 
-        alert("You won! \nYour Score: " + humanScore + "\nComputer Score:" + computerScore);
-        playRound(getHumanChoice(), getComputerChoice())
-
+        humanScore += 1;
+        console.log("human won"); 
     } else if (humanInput == "Paper" && computerInput == "Rock") {
         humanScore += 1; 
-        alert("You won! \nYour Score: " + humanScore + "\nComputer Score:" + computerScore);
-        playRound(getHumanChoice(), getComputerChoice())
+        console.log("human won"); 
 
     } else if (humanInput == "Scissors" && computerInput == "Paper") {
         humanScore +=1; 
-        alert("You won! \nYour Score: " + humanScore + "\nComputer Score:" + computerScore);
-        playRound(getHumanChoice(), getComputerChoice())
+        console.log("human won"); 
 
     } else {
         computerScore += 1;
-        alert("You Lost... \nYour Score: " + humanScore + "\nComputer Score:" + computerScore);
-        playRound(getHumanChoice(), getComputerChoice())
+        console.log("computer won"); 
 
     }
-
-
-
+    updateScores();
+    if (computerScore == 5) {
+        console.log("you lost");
+        finalText.textContent = "you lost";
+        canPlay = false;
+    } else if (humanScore == 5) {
+        console.log("you win!");
+        finalText.textContent = "you won!";
+        canPlay = false;
+    }   
 }
 
+const rock = document.querySelector("#rock");
+const paper = document.querySelector("#paper");
+const scissors = document.querySelector("#scissors");
 
-playRound(getHumanChoice(), getComputerChoice())
+
+rock.addEventListener("click", function() {
+
+    if (canPlay != false) {
+        humanInput = "Rock";
+        let computerChoice = getComputerChoice();
+        console.log("user chose rock");
+
+        playRound(humanInput,computerChoice);
+
+    }
+});
+
+paper.addEventListener("click", function() {
+    if (canPlay != false) {
+
+        humanInput = "Paper";
+        let computerChoice = getComputerChoice();
+        console.log("user chose paper");
+
+        playRound(humanInput,computerChoice);
+    }
+    
+
+});
+
+scissors.addEventListener("click", function() {
+    if (canPlay != false) {
+        humanInput = "Scissors";
+        let computerChoice = getComputerChoice(); 
+        console.log("user chose scissors");
+
+        playRound(humanInput,computerChoice);
+    
+    }
+
+});
+
+const results = document.querySelector("div");
+
+const dashboard = document.createElement("p");
+dashboard.textContent = "Pick an option... I am waiting for you first to five wins!";
+
+const humanText = document.createElement("p");
+humanText.textContent = `Human score: ${humanScore}`
+const computerText = document.createElement("p");
+computerText.textContent = `Computer score: ${computerScore}`
+const finalText = document.createElement("p");
+
+results.append(dashboard);
+results.appendChild(humanText);
+results.appendChild(computerText);
+results.appendChild(finalText);
+
+
+
+
 
 
 
